8dfefef1a6630726d89c94e30c3c07ad48964acc
Santi Espada <santi.espada@gmail.com>
2017-03-23 13:35:44 +0100

    studygroups can now be created

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from flask import request
 from models.connection import db
+from models import StudyGroup
 
 # Inicializacion del objeto Flask
 app = Flask(__name__)
@@ -15,6 +17,33 @@ db.init_app(app)
 @app.route('/')
 def index():
     return 'Hola mundo'
+
+
+@app.route('/studygroups', methods = ['POST'])
+def createStudyGroup():
+    data = request.json
+    studygroup = StudyGroup(int(data['study']), int(data['school']), data['subject'])
+
+    #Optional values
+    if 'date' in data.keys():
+        studygroup.data = data['date']
+
+    if 'description' in data.keys():
+        studygroup.description = data['description']
+
+    if 'place' in data.keys():
+        studygroup.place = data['place']
+
+    db.session.add(studygroup)
+    db.session.commit()
+
+    return studygroup.__repr__()
+
+
+@app.route('/studygroups', methods = ['GET'])
+def showStudyGroup():
+    #TODO
+    pass
 
 if __name__ == '__main__':
     app.run()
